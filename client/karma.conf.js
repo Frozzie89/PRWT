1097dc53d75349b5f03bab4123c22b41722af09b
Frozzie89 <froztor85@gmail.com>
2025-12-29 19:06:25 +0100

    add karma configuration for unit testing with Puppeteer support

---
+++ client/karma.conf.js
@@ -0,0 +1,59 @@
+// Karma configuration file
+// https://karma-runner.github.io/latest/config/configuration-file.html
+
+// Ensure Puppeteer's Chromium is used for CI / headless runs
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ],
+
+    client: {
+      jasmine: {
+        // Jasmine options can go here
+      },
+      clearContext: false, // keep Jasmine Spec Runner output visible
+    },
+
+    jasmineHtmlReporter: {
+      suppressAll: true,
+    },
+
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/client'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+      ],
+    },
+
+    reporters: ['progress', 'kjhtml'],
+
+    // CI-safe headless Chrome launcher
+    browsers: ['ChromeHeadlessCI'],
+
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-gpu',
+          '--disable-dev-shm-usage',
+        ],
+      },
+    },
+
+    restartOnFileChange: true,
+  });
+};
