ff7e424dccaaf27b96aecb4dcaecd8540210cd49
Frozzie89 <froztor85@gmail.com>
2025-12-30 22:34:46 +0100

    Refactor list-group component to use GroupsService for data management and update main-container interactions

--- client/src/app/components/main-container/main-container.component.ts
+++ client/src/app/components/main-container/main-container.component.ts
@@ -1,6 +1,8 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import { ListGroupComponent } from '../list-group/list-group.component';
 import { NewGroupComponent } from '../new-group/new-group.component';
+import { ListGroupItem } from '../../interfaces/list-group-item';
+import { GroupsService } from '../../service/groups.service';
 
 @Component({
   selector: 'app-main-container',
@@ -9,17 +11,27 @@ import { NewGroupComponent } from '../new-group/new-group.component';
   styleUrl: './main-container.component.scss',
 })
 export class MainContainerComponent {
+  groupsService = inject(GroupsService);
+
   isAdding = false;
 
-  openAdd() {
+  slideToAddForm() {
     this.isAdding = true;
   }
-  closeAdd() {
+
+  slideBack() {
     this.isAdding = false;
+  }
+
+  onCreate($event: ListGroupItem) {
+    this.groupsService.add($event);
+    this.slideBack();
   }
 
   @HostListener('document:keydown.escape')
   onEsc() {
-    if (this.isAdding) this.closeAdd();
+    if (this.isAdding) {
+      this.slideBack();
+    }
   }
 }
--- client/src/app/service/groups.service.ts
+++ client/src/app/service/groups.service.ts
@@ -7,8 +7,35 @@ import { ListGroupItem } from '../interfaces/list-group-item';
 export class GroupsService {
   private readonly _groups = signal<ListGroupItem[]>([]);
 
+  constructor() {
+    this._groups.set([
+      {
+        title: 'Item 1',
+        description: 'Description for Item 1',
+        icon: 'https://picsum.photos/200?random=' + Math.floor(Math.random() * 1000),
+        color: '#ffe600',
+      },
+      {
+        title: 'Item 2',
+        description: 'Description for Item 2',
+        icon: 'https://picsum.photos/200?random=' + Math.floor(Math.random() * 1000),
+        color: '#ff006e',
+      },
+      {
+        title: 'Item 3',
+        description: 'Description for Item 3',
+        icon: 'https://picsum.photos/200?random=' + Math.floor(Math.random() * 1000),
+        color: '#5aed1bff',
+      },
+    ]);
+  }
+
   readonly groups = this._groups.asReadonly();
   readonly count = computed(() => this._groups().length);
+
+  titleAlreadyExists(title: string) {
+    return this._groups().some(g => g.title === title);
+  }
 
   add(group: ListGroupItem) {
     this._groups.update(groups => [group, ...groups]);
