f5adaa13906e112daad1f96396edeeb88ca09b51
Frozzie89 <froztor85@gmail.com>
2025-12-30 12:27:35 +0100

    new listgroup component with placeholder items

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListGroupComponent } from './components/list-group/list-group.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListGroupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ client/src/app/components/list-group/list-group.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { ListGroup } from '../../types/list-group';
+
+@Component({
+  selector: 'app-list-group',
+  imports: [],
+  templateUrl: './list-group.component.html',
+  styleUrl: './list-group.component.scss',
+})
+export class ListGroupComponent {
+  listGroup: ListGroup = {
+    items: [
+      {
+        title: 'Item 1',
+        description: 'Description for Item 1',
+      },
+      {
+        title: 'Item 2',
+        description: 'Description for Item 2',
+      },
+      {
+        title: 'Item 3',
+        description: 'Description for Item 3',
+      },
+    ],
+  };
+}
