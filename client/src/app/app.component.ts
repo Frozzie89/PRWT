ddcf0ffa3fb2d7e418f0e3d84f5001580816d34c
Frozzie89 <froztor85@gmail.com>
2026-01-04 00:41:58 +0100

    feat: implement nav bar component and update routing

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MainContainerComponent } from './components/main-container/main-container.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainContainerComponent],
+  imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
