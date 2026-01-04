0824bb2959bae73f615e94c3f9e8a072f8e9f9be
Frozzie89 <froztor85@gmail.com>
2026-01-04 15:07:28 +0100

    implement view transitions

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,7 +4,7 @@ import {
   provideAppInitializer,
   provideZoneChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { PocketbaseAuthService } from './services/pocketbase-auth.service';
@@ -12,7 +12,7 @@ import { PocketbaseAuthService } from './services/pocketbase-auth.service';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withViewTransitions()),
     provideAppInitializer(() => {
       const auth = inject(PocketbaseAuthService);
       return auth.ensureDevAutoLogin();
--- client/src/app/components/list-group/list-group.component.ts
+++ client/src/app/components/list-group/list-group.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { GroupsService } from '../../services/groups.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-group',
@@ -9,10 +10,16 @@ import { GroupsService } from '../../services/groups.service';
 })
 export class ListGroupComponent implements OnInit {
   private readonly groupsService = inject(GroupsService);
+  private readonly router = inject(Router);
+
   readonly listGroup = this.groupsService.groups;
 
   ngOnInit() {
     void this.groupsService.load();
     this.groupsService.enableRealtime();
+  }
+
+  openGroup(groupId: string) {
+    this.router.navigate(['/groups', groupId]);
   }
 }
