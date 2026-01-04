6b4dba594ac70e6072d446cf073237b713427fd0
Frozzie89 <froztor85@gmail.com>
2026-01-04 14:29:23 +0100

    generated list-group-entries component

---
+++ client/src/app/components/list-group-entries/list-group-entries.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-group-entries',
+  imports: [],
+  templateUrl: './list-group-entries.component.html',
+  styleUrl: './list-group-entries.component.scss',
+})
+export class ListGroupEntriesComponent {
+
+}
