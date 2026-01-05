635355b02111782ae036514235cfa5451728bd0d
Frozzie89 <froztor85@gmail.com>
2026-01-05 23:51:33 +0100

    refactor: update list-group-entries component to use groupEntries service and remove placeholder entries

--- client/src/app/components/list-group-entries/list-group-entries.component.ts
+++ client/src/app/components/list-group-entries/list-group-entries.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs/operators';
+
+import { GroupEntriesService } from '../../services/group-entries.service';
+import { Group } from '../../interfaces/group';
 
 @Component({
   selector: 'app-list-group-entries',
@@ -8,42 +14,28 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './list-group-entries.component.scss',
 })
 export class ListGroupEntriesComponent {
+  private readonly route = inject(ActivatedRoute);
+  private readonly groupEntriesService = inject(GroupEntriesService);
+
+  private readonly groupFromRoute = toSignal(this.route.data.pipe(map(d => d['group'] as Group)), {
+    initialValue: null,
+  });
+
+  readonly group = computed(() => this.groupFromRoute());
+
+  readonly groupEntries = this.groupEntriesService.groupsEntries;
+
   newTextEntry = '';
 
-  // placeholder list of entries
-  entries = [
-    {
-      id: 1,
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vehicula eros vitae dignissim lobortis. Integer id efficitur tortor. Aenean vulputate neque ut dolor volutpat, nec commodo arcu fringilla. In hac habitasse platea dictumst. Maecenas cursus ullamcorper fermentum. Etiam pellentesque mattis lacus sit amet accumsan. Aliquam vulputate eu justo ut feugiat. Duis pulvinar placerat molestie. Phasellus commodo dictum sapien eu scelerisque. Ut mollis diam elit, quis congue felis scelerisque id. Aliquam urna diam, porttitor et turpis et, pellentesque porttitor leo. ',
-      totalVotes: 5,
-    },
-    {
-      id: 2,
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vehicula eros vitae dignissim lobortis.',
-      totalVotes: 3,
-    },
-    {
-      id: 3,
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vehicula eros vitae dignissim lobortis. Integer id efficitur tortor. Aenean vulputate neque ut dolor volutpat, nec commodo arcu fringilla. In hac habitasse platea dictumst. Maecenas cursus ullamcorper fermentum. Etiam pellentesque mattis lacus sit amet accumsan.',
-      totalVotes: -2,
-    },
-    {
-      id: 4,
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      totalVotes: 0,
-    },
-    {
-      id: 5,
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam consectetur sem a ullamcorper vehicula. Nulla at felis suscipit, malesuada tellus. ',
-      totalVotes: 12,
-    },
-  ];
-
   addEntry() {
-    this.entries.push({
-      id: this.entries.length + 1,
+    const group = this.group();
+    if (!group) {
+      return;
+    }
+
+    this.groupEntriesService.add({
+      groupId: group.id,
       text: this.newTextEntry,
-      totalVotes: 0,
     });
 
     this.newTextEntry = '';
